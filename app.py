0215365c1693af3929cb018fd312d97c1655f117
petrovich_z <petrovich.z@gmail.com>
2021-03-05 05:43:39 +0500

    simple tb bot

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from config import Config
+import telebot
+import emmet
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+bot = telebot.TeleBot(app.config['TGBOTAPI_TOKEN'])
+# bot.infinity_polling()
+
+@bot.message_handler(commands=["start"])
+def start_command(message):
+    # print(message)
+    bot.send_message(message.chat.id, 'Welcome! '+message.text)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    print(message.text)
+    bot.send_message(message.chat.id, emmet.expand(message.text))
+
+
+bot.polling()
 
 import routes
-
-if __name__ == '__main__':
-    app.run()
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TGBOTAPI_TOKEN = os.environ.get('TGBOTAPI_TOKEN') or ''
